f4917fb8f6eeffb75d613d0924bd85d049fd31fe
Edoardo Ierina <edoardo.ierina@gmail.com>
2024-10-25 15:48:20 -0300

    Fix next.config.js security

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cloud.umami.is eu.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app vercel.com *.umami.is cdn.jsdelivr.net *.vercel.com *.cloudflare.com *.mailchimp.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
